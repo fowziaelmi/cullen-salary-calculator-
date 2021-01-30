9a9ce3f6a633f2a56715d20013d0729ec212f463
Faduma Elmi <fowziiae@gmail.com>
2021-01-30 14:19:29 -0600

    Stored all input into an array

--- salarycalculator.js
+++ salarycalculator.js
@@ -3,3 +3,35 @@ $(document).ready(onReady);
 function onReady() {
   console.log('im ready');
 }
+let employeeData = [];
+// When submit button is pressed, it will take the input.
+//Store input into an array
+$(document).on('click', '#addItemButton', onAddItem);
+
+function onAddItem() {
+  console.log('in add item');
+  event.preventDefault();
+
+  //Grab input data from the Dome
+
+  let firstName = $('#firstNameInput').val();
+  let lastName = $('#lastNameInput').val();
+  let employeeId = $('#idInput').val();
+  let role = $('#roleInput').val();
+  let monthlySalary = $('#monthlySalaryInput').val();
+
+  // create an item object
+  let item = {
+    firstName: firstName,
+    lastName: lastName,
+    employeeId: Number(employeeId),
+    role: role,
+    monthlySalary: Number(monthlySalary),
+  };
+
+  console.log('got and item', item);
+
+  //push each item into an array
+
+  employeeData.push(item);
+}
